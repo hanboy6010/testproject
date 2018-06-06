4dba59d233937beefac08111cb761c03045c70ef
ju6579 <ju6579@naver.com>
2018-06-07 01:52:22 +0900

    project start 2

--- indian/models.py
+++ indian/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class GameSet(models.Model):
+    player_id = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     player1money = 100000
     player1CardNumber = 0
     player2money = 100000
@@ -10,3 +12,6 @@ class GameSet(models.Model):
     bankerMoney = 0
     regame = False
     waitForRequest = "Please, wait Other Player"
+
+    def __str__(self):
+        return self.player_id
--- indian/views.py
+++ indian/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import GameSet
 
+
 def index(request):
     return render(request, 'indian/index.html', {})
 
+
+@login_required
 def indian(request):
+    GameSet.player_id = request.user
     return render(request, 'indian/indian.html', {'game': GameSet})
 
 
