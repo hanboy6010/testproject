045a7614c578457e66b6212fb3c88888e5a77d0f
ju6579 <ju6579@naver.com>
2018-06-06 17:26:18 +0900

    project base 3

--- indian/models.py
+++ indian/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class GameSet(models.Model):
+    player1money = models.PositiveIntegerField(default=100000)
+    player1CardNumber = models.PositiveIntegerField()
+    player2money = models.PositiveIntegerField(default=100000)
+    player2CardNumber = models.PositiveIntegerField()
+    bankerMoney = models.PositiveIntegerField(default=0)
+    regame = models.BooleanField(default=True)
--- indian/views.py
+++ indian/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from .models import GameSet
 
 def index(request):
     return render(request, 'indian/index.html', {})
 
 def indian(request):
-    return render(request, 'indian/indian.html', {})
+    return render(request, 'indian/indian.html', {'game': GameSet})
